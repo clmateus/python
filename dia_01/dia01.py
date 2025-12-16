d117946a68e122ab1a7bf8c98ac94bb280471915
clmateus <cl.mateuscosta@gmail.com>
2025-12-16 10:39:59 -0300

    first commit

---
+++ dia_01/dia01.py
@@ -0,0 +1,18 @@
+# %%
+# Hello World e função print()
+print('Hello World!')
+print(7+4)
+print('7'+'4')
+print('Hello', 5)
+# %%
+# Variáveis
+nome = 'Mateus'
+idade = 25
+peso = 82.2
+print(nome, idade, peso)
+# %%
+# Função input()
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input('Quanto você pesa?')
+print(nome, idade, peso)
---
+++ dia_01/exercicios01.py
@@ -0,0 +1,30 @@
+# %%
+# Exercício 1
+nome = input('Qual é o seu nome?')
+print(f'Olá {nome}! Prazer em te conhecer!')
+# %%
+# Exercício 2
+dia = input('Digite o dia do seu nascimento: ')
+mes = input('Digite o mês do seu nascimento: ')
+ano = input('Digite o ano do seu nascimento: ')
+print(f'{dia}/{mes}/{ano}')
+# %%
+# Exercício 3
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print(f'{n1}+{n2}={n1+n2}')
+# %%
+# Exercício 4
+print(f'Olá, Mundo! Estou pronto para me tornar um programador.')
+# %%
+# Exercício 5
+nome = 'Mateus'
+idade = 25
+profissao = 'programador'
+print('Meu nome é', nome, 'tenho', idade, 'anos e sou', profissao)
+# %%
+# Exercício 6
+nome = input('Qual é o seu nome?')
+filme = input('Qual é o seu filme preferido?')
+print('Prazer', nome, '! O filme', filme, 'é excelente!')
+# %%
