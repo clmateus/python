8e1352cc07a10d1a6946952f94208a3c5eea8be4
clmateus <cl.mateuscosta@gmail.com>
2026-01-26 11:19:41 -0300

    reforço arquivos

--- dia_10/reforco_arquivos.py
+++ dia_10/reforco_arquivos.py
@@ -17,3 +17,19 @@ with open(file='./banco.csv', mode='r', encoding='utf-8') as f:
 for linha in conteudo:
     print(linha)
 # %%
+# Extração de valores de uma coluna específica
+idades = []
+
+with open(file='./banco.csv', mode='r', encoding='utf-8') as f:
+    linha = f.readline() # Lê o cabeçalho
+    linha = f.readline() # Lê a primeira linha
+
+    while linha:
+        linha_separada = linha.split(sep=',') # Quebra a string nas vírgulas e salva os resultados em uma lista
+        idade = linha_separada[0] # Seleciona o primeiro elemento da lista (neste caso é a coluna idade)
+        # idade = int(idade) # Transforma o valor idade de string para integer
+        idades.append(idade) # Salva o valor idade na lista de idades
+        linha = f.readline() # Lê a próxima linha. Caso ela não exista, salva o valor "None" que por sua vez encerra o laço de repetição
+
+print(idades)
+# %%
