63ec5c63b78278d59c2eb47393ba7222da6c99ea
clmateus <cl.mateuscosta@gmail.com>
2026-02-18 10:20:18 -0300

    classes

---
+++ Programação Orientada a Objetos/1. Classes/classes.py
@@ -0,0 +1,23 @@
+# %%
+# Definição de classe
+
+class NomeClasse(object):
+
+    def _init_(self, params):
+        self.atributo1 = ...
+        self.atributo2 = ...
+
+    def metodo1(self, params):
+        ...
+
+    def metodo2(self, params):
+        ...
+
+# %%
+# Exemplo de classe: Pessoa
+
+class Pessoa(object):
+
+    def _init_(self):
+        pass
+# %%
