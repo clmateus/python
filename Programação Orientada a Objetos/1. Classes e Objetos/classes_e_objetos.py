94ff9d6e007b1611bb8ef3d1aed089889aaf28f5
clmateus <cl.mateuscosta@gmail.com>
2026-02-20 12:09:26 -0300

    arquivos com classes

--- Programação Orientada a Objetos/1. Classes e Objetos/classes_e_objetos.py
+++ Programação Orientada a Objetos/1. Classes e Objetos/classes_e_objetos.py
@@ -61,3 +61,26 @@ print(type(joao))
 joao.dormir(horas=2)
 joao.falar(texto=f'Olá pessoal! Aqui é o {joao.nome}')
 # %%
+# Exemplo classe arquivo CSV
+class ArquivoCSV(object):
+    def __init__(self, arquivo: str):
+        self.arquivo = arquivo
+        self.conteudo = self._extrair_conteudo()
+        self.colunas = self._extrair_nome_colunas()
+
+        def _extrair_conteudo(self):
+            conteudo = None
+            with open(file=self.arquivo, mode='r', encoding='utf-8') as arquivo:
+                conteudo = arquivo.readlines()
+            return conteudo
+        
+        def _extrair_nome_colunas(self):
+            return self.conteudo[0].strip().split(sep=',')
+        
+        def extrair_coluna(self, indice_coluna: str):
+            coluna = list()
+            for linha in self.conteudo:
+                conteudo_linha = linhha.strip().split(sep=',')
+                coluna.append(conteudo_linha[indice_coluna])
+            coluna.pop(0)
+            return coluna
